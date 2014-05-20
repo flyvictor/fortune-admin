fa7ae9dbda4cdb027a356dba26d182a85e6f3516
EugeneKostrikov <eugene.kostrikov@gmail.com>
2014-05-20 20:17:20 +0400

    Resources view and controller

--- public/app/scripts/controllers/controllers.js
+++ public/app/scripts/controllers/controllers.js
@@ -6,26 +6,37 @@ var controllers = angular.module('fortuneAdmin.Controllers', [
   'fortuneAdmin.Controllers.umlDiagram'
 ]);
 
+controllers.filter('filterLinks', [function(){
+  return function(input){
+    var nonLinks = {};
+    angular.forEach(input, function(field, name){
+      if (!angular.isObject(field)){
+        nonLinks[name] = field;
+      }
+    });
+    return nonLinks;
+  }
+}]);
+
 controllers.controller('ResourcesCtrl', ['$scope', '$http', 'Inflect', '$routeParams', 'resources', 'data', ResourcesCtrl]);
 
 function ResourcesCtrl($scope, $http, Inflect, $routeParams, resources, data){
-  $scope.allResources = resources;
-  $scope.currentResource  = resources.find(function(res){
+  var currentResource  = resources.find(function(res){
     return res.name === $routeParams.name || Inflect.pluralize(res.name) === $routeParams.name;
   });
-  $scope.data = data[Inflect.pluralize($scope.currentResource.name)];
+  var plurResourceName = Inflect.pluralize(currentResource.name);
+  $scope.plurResourceName = plurResourceName;
+  $scope.currentResource = currentResource;
+  $scope.data = data[plurResourceName];
   $scope.links = data.links;
+  $scope.PK = $scope.currentResource.modelOptions ? $scope.currentResource.modelOptions.pk || 'id' : 'id';
 
   this.resolveInverse = function(linkName){
     var parts = linkName.split('.');
+    //No need to dig deeper as there's no nested schemas
     var fieldName = parts[parts.length - 1];
 
-    // Dig down through nested schemas
-    var iterator = $scope.currentResource.schema;
-    for (var i = 1; i < parts.length - 1; i++){
-      iterator = iterator[parts[i]];
-    }
-    var ref = iterator[fieldName];
+    var ref = currentResource.schema[fieldName];
 
     var inverse = '';
     if (angular.isArray(ref)){
@@ -38,6 +49,43 @@ function ResourcesCtrl($scope, $http, Inflect, $routeParams, resources, data){
     return inverse;
   };
 
+
+  this.addRow = function(Primary){
+    var newRow = {};
+    if ($scope.PK !== 'id'){
+      newRow[$scope.PK] = Primary;
+    }
+
+    if ($routeParams.id) {
+      // $scope.currentResource - what is going to be created
+      // $routeParams.inverse - field that references parent resource
+      var refFieldName = $routeParams.inverse;
+
+      //Create reference to new doc
+      if (angular.isArray($scope.currentResource.schema[refFieldName])){
+        newRow[refFieldName] = [$routeParams.id];
+      }else{
+        newRow[refFieldName] = $routeParams.id;
+      }
+    }
+    var cmd = {};
+    cmd[plurResourceName] = [newRow];
+    $http.post(CONFIG.getApiNamespace() + '/' + plurResourceName, cmd).success(function(data) {
+      $scope.data.push(data[plurResourceName][0]);
+    });
+  };
+
+  this.deleteRow = function(index, id){
+    $http.delete(CONFIG.getApiNamespace() + '/' + plurResourceName + '/' + id)
+      .success(function (data, status) {
+        console.log(status);
+        $scope.data.splice(index, 1);
+      })
+      .error(function(data, status) {
+        console.log(status);
+      });
+
+  }
 }
 
 controllers.controller('UsersCtrl',[ '$scope', '$filter', '$http', '$routeParams',
