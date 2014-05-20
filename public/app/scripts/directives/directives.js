ba5fba1264c09a319b4cebe642643b74634c3ea7
EugeneKostrikov <eugene.kostrikov@gmail.com>
2014-05-20 20:15:49 +0400

    Port to x-editable

--- public/app/scripts/directives/directives.js
+++ public/app/scripts/directives/directives.js
@@ -26,94 +26,38 @@ directives.directive('myNavbar', [ '$http', '$rootScope', function($http, $rootS
   }
 }]);
 
-directives.directive('faEditable', [function(){
+directives.directive('faEditable', ['$http', function($http){
   return {
     restrict: 'E',
     replace: true,
     scope: {
       value: '=ngModel',
       path: '=',
-      schemaType: '='
+      schemaType: '=',
+      resourceName: '@',
+      resourceId: '@'
     },
     templateUrl: '/templates/directives/faEditable.html',
     link: function(scope){
-      scope.isRef = false;
-      if (angular.isObject(scope.schemaType)){
-        scope.isRef = true;
-        if (angular.isArray(scope.schemaType)){
-          scope.multi = true;
-        }
-      }
 
-    }
-  }
-}]);
-
-directives.directive('faRef', ['$q', '$http', '$modal', function($q, $http, $modal){
-  return {
-    restrict: 'E',
-    replace: false,
-    scope: {
-      value: '=',
-      ref: '='
-    },
-    templateUrl: '/templates/directives/faRef.html',
-    link: function(scope, elt){
-      var refTo = angular.isArray(scope.ref) ? scope.ref[0].ref : scope.ref.ref;
-      var resources, currentResource;
-      $http.get(CONFIG.baseEndpoint + '/resources').success(function(data){
-        resources = data.resources;
-        currentResource = resources.find(function(resource){
-          return resource.name === refTo;
+      scope.apply = function(value){
+        //Pass new value from x-editable to ngModel
+        scope.value = value;
+        //And send PATCH to the server
+        var cmd = [];
+        cmd.push({
+          op: 'replace',
+          path: '/' + scope.resourceName + '/0/' + scope.path,
+          value: value
         });
-      });
-
-      function pluralize(str){
-        return /s$/.test(str) ? str + 'es' : str + 's';
-      }
-
-      scope.$watch('value', function(){
-        console.log('value changed');
-      });
-
-      elt.bind('click', function(){
-        //This will work only for resources with business PK.
-        //refs by _id need some alias or something to be at least readable
-        var modalTemplate = [
-          '<div> ',
-          '<p>Please, select ', currentResource.modelOptions.pk || 'ObjectId', '</p>',
-          '<select class="form-control"',
-          'ng-model="selected" ',
-          'ng-options="item.', currentResource.modelOptions.pk || 'ObjectId',
-          ' as item.', currentResource.modelOptions.pk || 'ObjectId',
-          ' for item in list" ',
-          'ng-change="onSelect(selected)"',
-          '></select> ',
-          '<pre>{{ list | json }}</pre>',
-          '</div>'
-        ];
-        $modal.open({
-          resolve: {
-            list: function(){
-              var d = $q.defer();
-              $http.get(CONFIG.getApiNamespace() + '/' + pluralize(refTo))
-                .success(function(data){
-                  d.resolve(data[pluralize(refTo)]);
-                });
-              return d.promise;
-            }
-          },
-          template: modalTemplate.join(''),
-          controller: ['$scope', '$modalInstance', 'list', function($scope, $modalInstance, list){
-            $scope.list = list;
-            $scope.onSelect = function(item){
-              scope.value = item;
-              console.log(item);
-              $modalInstance.close();
-            }
-          }]
+        $http({
+          method: 'PATCH',
+          url: CONFIG.getApiNamespace() + '/' + scope.resourceName + '/' + scope.resourceId,
+          data: cmd
+        }).success(function(data, status){
+          console.log(data, status);
         });
-      });
+      };
     }
   }
 }]);
