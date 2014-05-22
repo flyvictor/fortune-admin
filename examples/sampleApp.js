2bb5b5af6bbbf679d7bda01adc588e8ad1a5b95f
EugeneKostrikov <eugene.kostrikov@gmail.com>
2014-05-22 11:52:55 +0400

    Examples

--- examples/sampleApp.js
+++ examples/sampleApp.js
@@ -8,6 +8,9 @@ angular.module('sampleApp', [
         template: ['<div class="text-center">',
           '<h1>{{ hello }}</h1>',
           '<h2>I have awesome fortune-admin dependency!</h2>',
+
+          // Note ng-href there. fortune-admin has no otherwise rule
+          // so you should append /uml to the mount point.
           '<h3><a ng-href="/admin/uml">Navigate to admin interface</a></h3>',
           '</div>'].join(' '),
         controller: 'sampleCtrl'
@@ -23,6 +26,7 @@ angular.module('sampleApp', [
     //3. Mount plugin to any route of your choice
     fortuneAdminProvider.mountTo($routeProvider, '/admin');
 
+    //Your app should be in html5Mode at the moment
     $locationProvider.html5Mode(true);
   }])
   .controller('sampleCtrl', ['$scope', function($scope){
