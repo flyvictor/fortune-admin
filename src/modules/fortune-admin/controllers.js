1ab03621197b609db865f112c8b1a27618dff67b
Eugene Kostrikov <eugene.kostrikov@gmail.com>
2014-12-10 15:48:10 +0300

    Move table grid to directive

--- src/modules/fortune-admin/controllers.js
+++ src/modules/fortune-admin/controllers.js
@@ -41,35 +41,6 @@ angular.module('fortuneAdmin.Controllers', [
       $scope.parentResourceName = $routeParams.parent;
       $scope.parentId = $routeParams.id;
 
-      this.resolveFieldName = function(linkName){
-        //No need to dig deeper as there's no nested schemas
-        var parts = linkName.split('.');
-        return parts[parts.length - 1];
-      };
-
-      this.resolveInverse = function(linkName){
-        var fieldName = this.resolveFieldName(linkName);
-        var ref = currentResource.schema[fieldName];
-
-        var inverse = '';
-        if (angular.isArray(ref)){
-          inverse = ref[0].inverse;
-        }else if(angular.isObject(ref)){
-          inverse = ref.inverse;
-        }else {
-          throw new Error('Malformed reference');
-        }
-        return inverse;
-      };
-
-
-      this.linkToMany = function(linkName){
-        var fieldName = this.resolveFieldName(linkName);
-        var ref = currentResource.schema[fieldName];
-        return angular.isArray(ref);
-      };
-
-
       this.addRow = function(Primary){
         var newRow = {};
         if ($scope.PK !== 'id'){
@@ -130,33 +101,7 @@ angular.module('fortuneAdmin.Controllers', [
           });
       };
 
-      this.applyFilter = function(selected, fieldName, type){
-        switch (type){
-          case 'String':
-            //Derived from typeahead
-            $scope.filter['filter[' + fieldName + '][regex]'] = selected.model;
-            $scope.filter['filter[' + fieldName + '][options]'] = 'i';
-            break;
-          case 'Number':
-          case 'Date':
-            $scope.filter['filter[' + fieldName + '][gte]'] = selected.start;
-            $scope.filter['filter[' + fieldName + '][lte]'] = selected.end;
-            break;
-          case 'Boolean':
-            $scope.filter['filter[' + fieldName + ']'] = selected;
-            break;
-        }
-        runCurrentFilter();
-      };
-
-      this.dropFilter = function(fieldName){
-        delete $scope.filter['filter[' + fieldName + '][regex]'];
-        delete $scope.filter['filter[' + fieldName + '][options]'];
-        delete $scope.filter['filter[' + fieldName + '][gte]'];
-        delete $scope.filter['filter[' + fieldName + '][lte]'];
-        delete $scope.filter['filter[' + fieldName + ']'];
-        runCurrentFilter();
-      };
+      this.filterChangedCb = runCurrentFilter();
 
       function runCurrentFilter(){
         $http.get(CONFIG.fortuneAdmin.getApiNamespace() + '/' + plurResourceName,{
--- src/modules/fortune-admin/directives.js
+++ src/modules/fortune-admin/directives.js
@@ -1,5 +1,73 @@
 'use strict';
 angular.module('fortuneAdmin.Directives', [])
+  .directive('faGrid', [function(){
+    return {
+      restrict: 'E',
+      scope: {
+        data: '=',
+        links: '=',
+        resources: '=',
+        currentResource: '=',
+        filter: '=',
+        filterChangedCb: '&'
+      },
+      templateUrl: CONFIG.fortuneAdmin.prepareViewTemplateUrl('directives/faGrid'),
+      link: function(scope){
+        scope.dropFilter = function(fieldName, taQuery){
+          delete scope.filter['filter[' + fieldName + '][regex]'];
+          delete scope.filter['filter[' + fieldName + '][options]'];
+          delete scope.filter['filter[' + fieldName + '][gte]'];
+          delete scope.filter['filter[' + fieldName + '][lte]'];
+          delete scope.filter['filter[' + fieldName + ']'];
+        };
+
+        scope.applyFilter = function(selected, fieldName, type){
+          switch (type){
+            case 'String':
+              //Derived from typeahead
+              scope.filter['filter[' + fieldName + '][regex]'] = selected.model;
+              scope.filter['filter[' + fieldName + '][options]'] = 'i';
+              break;
+            case 'Number':
+            case 'Date':
+              scope.filter['filter[' + fieldName + '][gte]'] = selected.start;
+              scope.filter['filter[' + fieldName + '][lte]'] = selected.end;
+              break;
+            case 'Boolean':
+              scope.filter['filter[' + fieldName + ']'] = selected;
+              break;
+          }
+        };
+
+        scope.resolveFieldName = function(linkName){
+          //No need to dig deeper as there's no nested schemas
+          var parts = linkName.split('.');
+          return parts[parts.length - 1];
+        };
+
+        scope.resolveInverse = function(linkName){
+          var fieldName = this.resolveFieldName(linkName);
+          var ref = scope.currentResource.schema[fieldName];
+
+          var inverse = '';
+          if (angular.isArray(ref)){
+            inverse = ref[0].inverse;
+          }else if(angular.isObject(ref)){
+            inverse = ref.inverse;
+          }else {
+            throw new Error('Malformed reference');
+          }
+          return inverse;
+        };
+
+        scope.linkToMany = function(linkName){
+          var fieldName = this.resolveFieldName(linkName);
+          var ref = scope.currentResource.schema[fieldName];
+          return angular.isArray(ref);
+        };
+      }
+    }
+  }])
   .directive('faEditable', [function(){
     return {
       restrict: 'E',
