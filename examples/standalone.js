0bd9c08b76f55e94bf7a97e547e0878a5a207243
Alex McFadyen <alex@demotix.com>
2015-02-02 13:59:19 +0000

    Adding limited support for linked objects.
    Code needs moving around and it has implications for edit code - but its a working example.

--- examples/standalone.js
+++ examples/standalone.js
@@ -27,8 +27,51 @@
               });
             }],
             users: ['$http', function($http){
-              return $http.get('/api/v1/users').then(function(res){
-                return res.data.users;
+              return $http.get('/api/v1/users?include=addresses').then(function(res){
+                // Building the returned object into something simpler to work with
+                // So linked resources appear on the documents to which they relate
+
+                // This should likely be somewhere else.
+                /**
+                 * UnFlatterns the res to attach linked resources to their parents.
+                 * @param  {Object} res       HTTP get request result
+                 * @param  {String} singular  Name of the parent resource, singular
+                 * @param  {String} plural    Name of the parent resource, plural
+                 * @return {Object}           Formatted parent object
+                 */
+                var attachLinkedToRes = function (res, singular, plural) {
+                  var primaryResource = res.data[plural];
+
+                  // Build a resource indexed version of the linked resources
+                  var linkedIndexed = {};
+
+                  Object.keys(res.data.linked).forEach(function(linkedKey) {
+
+                    res.data.linked[linkedKey].forEach(function(linkedObject) {
+
+                      if (!linkedIndexed[linkedObject.links[singular]]) {
+                        linkedIndexed[linkedObject.links[singular]] = {};
+                      }
+
+                      if (!linkedIndexed[linkedObject.links[singular]][linkedKey]) {
+                        linkedIndexed[linkedObject.links[singular]][linkedKey] = [];
+                      }
+
+                      linkedIndexed[linkedObject.links[singular]][linkedKey].push(linkedObject);
+                    });
+                  });
+
+                  // Attach the linked resources to the primary resource
+                  primaryResource.forEach(function(primaryResourceDoc) {
+                    primaryResourceDoc.linked = linkedIndexed[primaryResourceDoc.id];
+                  });
+
+                  return primaryResource;
+                };
+
+                var data = attachLinkedToRes(res, 'user', 'users');
+
+                return data;
               });
             }],
             addresses: ['$http', function($http){
@@ -68,7 +111,7 @@
           }
         }
       ]);
-      
+
       $scope.startDocs = function(){
         docs.setApiHost($scope.params.host);
         docs.setApiNamespace($scope.params.namespace);
@@ -79,7 +122,7 @@
       $scope.startFA = function(){
         fortuneAdmin.setApiHost($scope.params.host);
         fortuneAdmin.setApiNamespace($scope.params.namespace);
-        
+
         $location.url('/admin/uml');
       };
     }])
